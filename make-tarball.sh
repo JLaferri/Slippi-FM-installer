f94a30e6c1ecdce179d7ebfea7e292b09560d8e3
meta <hosaka-corp@users.noreply.github.com>
2019-01-13 21:59:07 -0600

    update configs for r11 release
    - start tracking latest configs in this repo
    - playback config currently has "Render To Main Window" disabled
    - remove old r10 config filename

--- make-tarball.sh
+++ make-tarball.sh
@@ -1,15 +1,26 @@
-#!/bin/bash
+#!/bin/bash -e
 
 if [[ -z $1 ]]; then
-	echo "usage: ./make-tarball.sh <ouput archive filename>"
-	exit -1
+	echo "usage: ./make-tarball.sh <release name>"
+	echo -e "\t ie. ./make-tarball.sh r11"
+	exit 0
+fi
+
+if [[ $1 == "--help" ]] || [[ $1 == "-h" ]]; then
+	echo "usage: ./make-tarball.sh <release name>"
+	echo -e "\t ie. ./make-tarball.sh r11"
+	exit 0
 fi
 
 
-if [[ -e ${1} ]]; then
-	echo "${1} already exists, quitting"
+RECORDING_TAR="slippi-${1}-recording-config.tar.gz"
+PLAYBACK_TAR="slippi-${1}-playback-config.tar.gz"
+
+if [[ -e "$RECORDING_TAR" ]] || [[ -e "$PLAYBACK_TAR" ]]; then
+	echo "A configuration is already packaged for this release."
 	exit -1
 fi
 
-# Move the config/ directory contents into a gzip tarball
-tar -C config/ -cvzf "${1}" Binaries/
+# Move the config/ directory contents into gzip tarballs
+tar -C config/recording -cvzf "${RECORDING_TAR}" Binaries/
+tar -C config/playback -cvzf "${PLAYBACK_TAR}" Binaries/
